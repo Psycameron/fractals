706c306bb1018bb4a9c76ce055f145f930b29ee2
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-10-24 21:37:27 +0300

    feat(): Added the first point logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 import { Canvas } from "./components/Canvas/Canvas";
 
 function App() {
+  const [point, setPoint] = useState([]);
   const [value, setValue] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
   function valueChange(e) {
     setValue(e.target.value);
@@ -16,10 +18,20 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">Fractals maker</h1>
-      <Canvas value={value} />
+      {!isActive ? (
+        <h2>Draw the first point inside the figure</h2>
+      ) : (
+        <h2>Select the number of new points to generate</h2>
+      )}
+      <Canvas
+        value={value}
+        setPoint={setPoint}
+        isActive={isActive}
+        setIsActive={setIsActive}
+      />
       <form onSubmit={handleSubmit}>
         <input type="number" min="0" value={value} onChange={valueChange} />
-        <button className="startButton" type="submit">
+        <button className="startButton" type="submit" disabled={!isActive}>
           Generate points
         </button>
       </form>
--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,9 +1,9 @@
 import { useRef, useEffect, useState } from "react";
 import { getRandomIntInclusive } from "../../utils/Random";
 
-export function Canvas({ value }) {
-  const [x, setX] = useState(null);
-  const [y, setY] = useState(null);
+export function Canvas({ value, setPoint, isActive, setIsActive }) {
+  // const [x, setX] = useState(null);
+  // const [y, setY] = useState(null);
 
   const canvasRef = useRef(null);
 
@@ -21,7 +21,26 @@ export function Canvas({ value }) {
     drawPoint(250, 20);
     drawPoint(20, 480);
     drawPoint(480, 480);
-  }, []);
+
+    function handleClick(e) {
+      console.log("first");
+      const x = e.clientX - canvas.getBoundingClientRect().left;
+      const y = e.clientY - canvas.getBoundingClientRect().top;
+      drawPoint(x, y);
+      setPoint([{ x, y }]);
+      setIsActive(!isActive);
+
+      canvas.removeEventListener("click", handleClick);
+    }
+
+    if (!isActive) {
+      canvas.addEventListener("click", handleClick);
+    }
+
+    if (isActive) {
+      canvas.removeEventListener("click", handleClick);
+    }
+  }, [isActive, setIsActive, setPoint]);
 
   //   useEffect(() => {
   //     const canvas = canvasRef.current;
@@ -36,7 +55,11 @@ export function Canvas({ value }) {
 
   //   }, []);
 
-  return <canvas ref={canvasRef} width={500} height={500} />;
+  return (
+    <>
+      <canvas ref={canvasRef} width={500} height={500} />
+    </>
+  );
 }
 
 export {};
