d690f82be50b0e329d75b095e82ccc0da1b667c3
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-10-24 18:09:43 +0300

    chore(): init TS project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Fractals maker</h1>
+
+      <button className="startButton" type="button">
+        Create 3 start points
+      </button>
+    </div>
+  );
+}
+
+export default App;
